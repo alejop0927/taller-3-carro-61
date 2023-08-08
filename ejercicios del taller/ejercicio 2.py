07f641e34a61ede8badfb29a0af9f0540d1add34
Jpenuela420 <penuelaherrerajuanjose@gmail.com>
2023-08-08 13:31:59 -0500

    ejercicio2.2

--- ejercicios del taller/ejercicio 2.py
+++ ejercicios del taller/ejercicio 2.py
@@ -1,5 +1,48 @@
 # Cree una clase Punto que represente un punto en el plano cartesiano.
-class punto:
+class Punto:
     def __init__(self, x, y):
         self.x: float = x
         self.y: float = y
+
+
+
+
+#A la clase del punto anterior, defínale los siguientes métodos:
+#- Un método mostrar que imprima por consola las coordenadas del punto
+
+
+    def mostrar(self):
+        print(f"Coordenadas:({self.x}, {self.y})")
+
+
+#- Un método mover que cambie las coordenadas del punto
+
+    def mover(self,newx,newy):
+        self.x=newx
+        self.y=newy
+
+
+
+
+#- Un método calcular_distancia que calcule la distancia de la instancia actual con otro punto.
+
+     def calcular(self,operaion):
+
+operacionx =x-newx
+operaciony =y-newy
+       print(operacion)
+
+
+
+         punto1 = Punto(3, 4)
+
+         punto1.mostrar()
+
+         punto1.mover(7, 8)
+
+         punto1.mostrar()
+
+
+
+
+
